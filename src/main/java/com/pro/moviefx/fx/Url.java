aff60257855b8a377acad6d7bc52370432659639
Fabio <yfabioyasuo@gmail.com>
2024-11-12 10:07:40 -0300

    renamed fxml files

--- src/main/java/com/pro/moviefx/fx/Url.java
+++ src/main/java/com/pro/moviefx/fx/Url.java
@@ -2,8 +2,12 @@ package com.pro.moviefx.fx;
 
 public enum Url {
 	
-	MAIN("/fxml/main.fxml"), HOME("/fxml/home.fxml"), CARD_MOVIES("/fxml/card-movies.fxml"),
-	CARD_TVS("/fxml/card-tvs.fxml"), MOVIE("/fxml/movie.fxml"), TV("/fxml/tv.fxml");
+	MAIN("/fxml/main.fxml"),
+	HOME("/fxml/home.fxml"),
+	CARD_MOVIES("/fxml/cardMovies.fxml"),
+	CARD_TVS("/fxml/cardTvs.fxml"),
+	MOVIE("/fxml/movie.fxml"),
+	TV("/fxml/tv.fxml");
 
 	private String path;
 
